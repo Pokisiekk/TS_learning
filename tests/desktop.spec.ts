4fa6388f43f1d2643d2f4d6ec8d0f81cf1796877
Kamil Pokornicki <kamilpokornicki11@gmail.com>
2025-04-27 15:42:40 +0200

    tests/desktop.spec.ts: add phone top-up test
    Signed-off-by: Kamil Pokornicki <kamilpokornicki11@gmail.com>

--- tests/desktop.spec.ts
+++ tests/desktop.spec.ts
@@ -16,4 +16,18 @@ test.describe('My desktop page', () => {
     await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Michael Scott - 200,00PLN - Pożyczka');
   });
 
+  test('Phone top-up', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').fill('testerrr');
+    await page.getByTestId('password-input').fill('testerrr');
+    await page.getByTestId('login-button').click();
+    await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
+    await page.locator('#widget_1_topup_amount').fill('50');
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    await expect(page.getByTestId('message-text')).toHaveText('Doładowanie wykonane! 50,00PLN na numer 500 xxx xxx');
+  });
+
 });
