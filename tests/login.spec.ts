e3a5e8c53a2c338ade702b1b03c129a9c98a0021
Kamil Pokornicki <kamilpokornicki11@gmail.com>
2025-04-27 16:59:32 +0200

    tests/login.spec.ts: changes after usage of prettier
    Signed-off-by: Kamil Pokornicki <kamilpokornicki11@gmail.com>

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Login to Demobank', () => {
-  const url = 'https://demo-bank.vercel.app/'
-  const eightCharacters = 'testerrr'
-  const fourCharacters = 'test'
+  const url = 'https://demo-bank.vercel.app/';
+  const eightCharacters = 'testerrr';
+  const fourCharacters = 'test';
 
   test('Login with correct credentials', async ({ page }) => {
-    const expectedUser = 'Jan Demobankowy'
+    const expectedUser = 'Jan Demobankowy';
 
     await page.goto(url);
     await page.getByTestId('login-input').fill(eightCharacters);
@@ -17,24 +17,27 @@ test.describe('Login to Demobank', () => {
   });
 
   test('Login with incorrect username', async ({ page }) => {
-    const expectedWarning = 'identyfikator ma min. 8 znaków'
+    const expectedWarning = 'identyfikator ma min. 8 znaków';
 
     await page.goto(url);
     await page.getByTestId('login-input').fill(fourCharacters);
     await page.getByTestId('login-input').blur();
 
-    await expect(page.getByTestId('error-login-id')).toHaveText(expectedWarning);
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      expectedWarning,
+    );
   });
 
   test('Login with incorrect password', async ({ page }) => {
-    const expectedWarning = 'hasło ma min. 8 znaków'
+    const expectedWarning = 'hasło ma min. 8 znaków';
 
     await page.goto(url);
     await page.getByTestId('login-input').fill(eightCharacters);
     await page.getByTestId('password-input').fill(fourCharacters);
     await page.getByTestId('password-input').blur();
 
-    await expect(page.getByTestId('error-login-password')).toHaveText(expectedWarning);
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      expectedWarning,
+    );
   });
-
 });
