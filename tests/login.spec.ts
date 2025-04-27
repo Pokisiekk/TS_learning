3e0d8c2cdc911c024ec5890a3f233f744ddd90e7
Kamil Pokornicki <kamilpokornicki11@gmail.com>
2025-04-27 17:43:21 +0200

    tests/login.spec.ts: add beforeEach
    Signed-off-by: Kamil Pokornicki <kamilpokornicki11@gmail.com>

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,14 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Login to Demobank', () => {
-  const url = 'https://demo-bank.vercel.app/';
   const eightCharacters = 'testerrr';
   const fourCharacters = 'test';
+
+  test.beforeEach( async ({ page }) => {
+    const url = 'https://demo-bank.vercel.app/';
+    await page.goto(url);
+  });
 
   test('Login with correct credentials', async ({ page }) => {
     const expectedUser = 'Jan Demobankowy';
 
-    await page.goto(url);
     await page.getByTestId('login-input').fill(eightCharacters);
     await page.getByTestId('password-input').fill(eightCharacters);
     await page.getByTestId('login-button').click();
@@ -19,7 +22,6 @@ test.describe('Login to Demobank', () => {
   test('Login with incorrect username', async ({ page }) => {
     const expectedWarning = 'identyfikator ma min. 8 znaków';
 
-    await page.goto(url);
     await page.getByTestId('login-input').fill(fourCharacters);
     await page.getByTestId('login-input').blur();
 
@@ -31,7 +33,6 @@ test.describe('Login to Demobank', () => {
   test('Login with incorrect password', async ({ page }) => {
     const expectedWarning = 'hasło ma min. 8 znaków';
 
-    await page.goto(url);
     await page.getByTestId('login-input').fill(eightCharacters);
     await page.getByTestId('password-input').fill(fourCharacters);
     await page.getByTestId('password-input').blur();
