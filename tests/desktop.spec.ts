08841c0433acc4ee7f70b83e3683ca95ac769962
Kamil Pokornicki <kamilpokornicki11@gmail.com>
2025-05-04 13:25:23 +0200

    tests/desktop.spec.ts: add balance check test
    Signed-off-by: Kamil Pokornicki <kamilpokornicki11@gmail.com>

--- tests/desktop.spec.ts
+++ tests/desktop.spec.ts
@@ -41,4 +41,21 @@ test.describe('My desktop page', () => {
       `Doładowanie wykonane! ${amount},00PLN na numer ${phoneNumber}`,
     );
   });
+
+  test('Balance check after phone top-up', async ({ page }) => {
+    const phoneNumber = '500 xxx xxx';
+    const amount = '50';
+    const initBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initBalance) - Number(amount);
+
+    await page.locator('#widget_1_topup_receiver').selectOption(phoneNumber);
+    await page.locator('#widget_1_topup_amount').fill(amount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    await expect(page.locator('#money_value')).toHaveText(
+      `${expectedBalance}`,
+    );
+  });
 });
