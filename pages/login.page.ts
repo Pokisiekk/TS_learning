af240b65a7cbb7e24ebdf10c16f0ae509ad3001c
Kamil Pokornicki <kamilpokornicki11@gmail.com>
2025-06-12 17:33:04 +0200

    pages/login.page.ts: update LoginPage to allow login
    Signed-off-by: Kamil Pokornicki <kamilpokornicki11@gmail.com>

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,3 +1,9 @@
-class LoginPage {
-  constructor(parameters) {}
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  loginInput = this.page.getByTestId('login-input');
+  passwordInput = this.page.getByTestId('password-input');
+  loginButton = this.page.getByTestId('login-button');
 }
