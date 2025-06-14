e9528c30b8827c95afb72a27da2ef3629b1f0ff1
Kamil Pokornicki <kamilpokornicki11@gmail.com>
2025-06-14 10:27:44 +0200

    pages/payments.page.ts: add makeTransfer function

--- pages/payments.page.ts
+++ pages/payments.page.ts
@@ -11,4 +11,16 @@ export class PaymentsPage {
   transferButton = this.page.getByRole('button', { name: 'wykonaj przelew' });
   closeButton = this.page.getByTestId('close-button');
   messageLabel = this.page.getByTestId('message-text');
+
+  async makeTransfer(
+    receiverName: string,
+    receiverAccount: string,
+    amount: string,
+  ): Promise<void> {
+    await this.receiverName.fill(receiverName);
+    await this.receiverAccount.fill(receiverAccount);
+    await this.transferAmount.fill(amount);
+    await this.transferButton.click();
+    await this.closeButton.click();
+  }
 }
